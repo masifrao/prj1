ba0cb79b8a79c90d3db98bcd522f3a29473f7da9
masifrao <masif.rao@gmail.com>
2020-08-17 20:01:03 -0400

    Chapter1

--- Chapter1/crackpwd.py
+++ Chapter1/crackpwd.py
@@ -1,23 +1,24 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
 import crypt
 def testpass(cryptPass):
     salt = cryptPass[0:2]
     dictFile = open('dictionary.txt','r')
     for word in dictFile.readlines():
         word = word.strip('\n')
-        cryptword = crypt.crypt('word','salt')
+        cryptword = crypt.crypt(word, salt)
         if (cryptword == cryptPass):
             print '[+] Found Password: ' + word + "\n"
             return
-        print '[-] Password not found: '
-        return
+    print '[-] Password not found: '
+    return
 def main():
-    passfile = open('/opt/passwd')
+    passfile = open('password.txt')
     for line in passfile.readlines():
         if ":" in line:
             user =line.split(':')[0]
-            cryptpass = line.split(':')[1].strip(' ')
+            cryptPass = line.split(':')[1].strip(' ')
             print "[*] Cracking Password for: "+user
-            testpass(cryptpass)
+            testpass(cryptPass)
 if __name__ == "__main__":
     main()
